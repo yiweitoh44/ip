ef683fec3b5acf6331ee5dbd96cd35290dbbcbdb
DESKTOP-8BMV6Q7\Yi Wei Toh <yiweitoh44@gmail.com>
2026-01-26 16:56:31 +0800

    Level-2: Add tasks, list tasks, and exit on bye

--- src/main/java/Zack.java
+++ src/main/java/Zack.java
@@ -7,15 +7,24 @@ public class Zack {
         System.out.println("Howdy! I'm Zack");
         System.out.println("What can I do for you?");
 
+        String[] tasks = new String[100]; // fixed size array
+        int taskCount = 0;
+
         while (true) {
             String input = in.nextLine();
 
             if (input.equals("bye")) {
                 System.out.println("Aite ciao!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
             }
-
-            System.out.println(input);
         }
 
     }
